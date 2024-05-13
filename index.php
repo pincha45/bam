1f696f8eb5541cc9b67be729f4b2652f158abdf0
pincha45 <164159755+pincha45@users.noreply.github.com>
2024-05-13 15:15:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-hi
-sasasa
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>FlickFinder</title>
+  <link rel="icon" type="image" href="image/FlixFD.png"> 
+  <!--Unicon-->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+<body>
+</body>
+</html>
